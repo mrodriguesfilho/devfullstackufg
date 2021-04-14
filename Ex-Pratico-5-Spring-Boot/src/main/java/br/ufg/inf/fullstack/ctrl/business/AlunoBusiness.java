0b26e7c8dcf9837c1d57ba17ffe2647d22dcde64
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-04-14 00:31:17 -0300

    CRUDO Aluno e Curso Finalizado

---
+++ Ex-Pratico-5-Spring-Boot/src/main/java/br/ufg/inf/fullstack/ctrl/business/AlunoBusiness.java
@@ -0,0 +1,44 @@
+package br.ufg.inf.fullstack.ctrl.business;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ufg.inf.fullstack.model.entities.Aluno;
+import br.ufg.inf.fullstack.model.repositories.AlunoRepository;
+
+@Service
+public class AlunoBusiness {
+
+	@Autowired
+	private AlunoRepository repository;
+	
+	public List<Aluno> findAll(){
+		return repository.findAll();
+	}
+	
+	public Aluno findById(Integer id) {
+		Optional<Aluno> retorno = repository.findById(id);
+		return retorno.get();
+	}
+	
+	public Aluno insert(Aluno aluno) {
+		return repository.save(aluno);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public Aluno update(Aluno alunoUpd) {
+		Aluno aluno = repository.findById(alunoUpd.getIdAluno()).get();
+    aluno.setDtInicio(alunoUpd.getDtInicio());
+    aluno.setAtivo(alunoUpd.getAtivo());
+    aluno.setPessoa(alunoUpd.getPessoa());
+    aluno.setCurso(alunoUpd.getCurso());
+
+		return repository.save(aluno);
+	}
+}
